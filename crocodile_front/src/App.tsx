efca5883612480f91a885e86509156f029c5454b
devofy <artemijv65@gmail.com>
2023-09-27 22:40:14 +0500

    minor

--- crocodile_front/src/App.tsx
+++ crocodile_front/src/App.tsx
@@ -1,14 +1,18 @@
 import { FC } from "react";
-import { Container } from "@mui/material";
-import { Route, Routes } from "react-router-dom";
+import { Container, ThemeProvider } from "@mui/material";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { SignUp } from "./pages/sign_up/SignUp.tsx";
+import { theme } from "./theme.ts";
 
 export const App: FC = () => {
   return (
-    <Container>
-      <Routes>
-        <Route path={"/sign-up"} element={<SignUp />} />
-      </Routes>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Routes>
+          <Route path={"/sign-up"} element={<SignUp />} />
+          <Route path={"/"} element={<Navigate to={"/sign-up"} />} />
+        </Routes>
+      </Container>
+    </ThemeProvider>
   );
 };
